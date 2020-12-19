aeb628e206a425ec7af3a15396bdb77248877067
Greyson Murray <greyson.murray@gmail.com>
2020-12-19 10:14:18 -0500

    add project_id to urlpatterns

--- src/greysonmurray/urls.py
+++ src/greysonmurray/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 from greysonmurray import views
 
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	path('<int:project_id>/', views.project, name="project")
 ]
