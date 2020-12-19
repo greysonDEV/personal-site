67d111a60c94ae20f61d7f9d173762bf3c27b690
Greyson Murray <greyson.murray@gmail.com>
2020-12-19 10:25:33 -0500

    specify app_name

--- src/greysonmurray/urls.py
+++ src/greysonmurray/urls.py
@@ -5,6 +5,7 @@ src/greysonmurray/urls.py
 from django.urls import path
 from greysonmurray import views
 
+app_name = "projects"
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('<int:project_id>/', views.project, name="project")
